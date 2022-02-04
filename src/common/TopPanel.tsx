042759db846f55babee36c6e39713db64c2f7d03
Justin Seda <jseda3237@gmail.com>
2022-02-04 09:30:48 -0500

    Top Panel
    Added component for the top

--- src/common/TopPanel.tsx
+++ src/common/TopPanel.tsx
@@ -1,5 +1,23 @@
 const TopPanel = () => {
-	return <div></div>;
+	return (
+		<div>
+			<div className="row">
+				<div className="col-12">
+					<div className="div-right">
+						<button className="saveBtn">Save</button>
+						<button className="deleteBtn">Delete</button>
+					</div>
+					<div id="notepad-title">Notepad Title</div>
+
+					<input
+						id="notepad-title-input"
+						type="text"
+						placeholder="My notepad title.."
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default TopPanel;
