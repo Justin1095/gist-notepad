46135c74c0e4d3966b11107262bf06888dba7bd8
Justin Seda <jseda3237@gmail.com>
2022-02-04 09:24:28 -0500

    Notes component
    Added new notes component

--- src/common/NoteSection.tsx
+++ src/common/NoteSection.tsx
@@ -7,7 +7,32 @@ interface Props {
 }
 
 const NoteSection = ({ values, includeAddBtn, includeDeleteBtn }: Props) => {
-	return <div></div>;
+	return (
+		<div>
+			<div className="row">
+				<div className="col-4">
+					<input
+						type="text"
+						// value={values?.filename || ""}
+						placeholder="Enter note title..."
+						defaultValue={values?.filename || ""}
+					/>
+					{includeDeleteBtn && <button className="deleteBtn">Delete</button>}
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-4">
+					<textarea
+						rows={4}
+						// value={values?.content || ""}
+						placeholder="Enter note..."
+						defaultValue={values?.content || ""}
+					/>
+				</div>
+			</div>
+			{includeAddBtn && <button className="addBtn">Add</button>}
+		</div>
+	);
 };
 
 export default NoteSection;
