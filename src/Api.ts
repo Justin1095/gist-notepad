68c081b75d131d1d2101e46d55ef61227d05a4ec
Justin Seda <jseda3237@gmail.com>
2022-02-06 11:23:42 -0500

    Changed to gistId

--- src/Api.ts
+++ src/Api.ts
@@ -5,11 +5,11 @@ import { Note } from "./types";
 const octokit = new Octokit({
 	auth: process.env.REACT_APP_GITHUB_GIST_ACCESS_KEY,
 });
-const gistKey = "5e82aa03b8272a6b590e773777142cea";
+const gistId = "5e82aa03b8272a6b590e773777142cea";
 
 export const fetchGists = async () => {
 	const reponse = await octokit.request("GET /gists/{gist_id}", {
-		gist_id: gistKey,
+		gist_id: gistId,
 	});
 
 	return reponse.data;
@@ -17,7 +17,7 @@ export const fetchGists = async () => {
 
 export const postGist = async (note: Note) => {
 	await octokit.request("POST /gists/{gist_id}", {
-		gist_id: gistKey,
+		gist_id: gistId,
 		files: {
 			name: {
 				filename: note.filename,
@@ -29,7 +29,7 @@ export const postGist = async (note: Note) => {
 
 export const deleteGist = async (filename: string) => {
 	await octokit.request(`PATCH /gists/{gist_id}`, {
-		gist_id: gistKey,
+		gist_id: gistId,
 		files: {
 			[filename]: {},
 		},
@@ -40,7 +40,7 @@ export const deleteGist = async (filename: string) => {
 export const deleteAllGist = async (notes: Note[]) => {
 	notes.forEach(async (note: Note) => {
 		await octokit.request("PATCH /gists/{gist_id}", {
-			gist_id: gistKey,
+			gist_id: gistId,
 			files: {
 				[note.filename]: {},
 			},
@@ -53,13 +53,13 @@ export const updateGist = async (title: string, notes: Note[]) => {
 	// console.log(notes);
 
 	await octokit.request(`PATCH /gists/{gist_id}`, {
-		gist_id: gistKey,
+		gist_id: gistId,
 		description: title,
 	});
 
 	notes.forEach(async (note: Note) => {
 		await octokit.request("PATCH /gists/{gist_id}", {
-			gist_id: gistKey,
+			gist_id: gistId,
 			files: {
 				[note.filename]: {
 					filename: note.filename,
